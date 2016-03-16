626e405914e55631fb5b918a1ac314ae61c2e974
tianyl1984 <tianyl1984@qq.com>
2016-03-16 16:35:08 +0800

    add cjson demo

---
+++ lua/cjson_demo.lua
@@ -0,0 +1,19 @@
+local cjson = require("cjson")
+local obj = {
+	name = "zhangsan",
+	age = 23,
+	gf = nil
+}
+
+local str = cjson.encode(obj)
+ngx.say("<hr>")
+ngx.say(str, "<hr>")
+
+local str2 = '{"name":"lisi","age":24,"male":true,"bf":null}'
+local jsonObj = cjson.decode(str2)
+ngx.say(jsonObj.name,"<br>")
+ngx.say(jsonObj.age,"<br>")
+ngx.say(jsonObj.male,"<br>")
+ngx.say(jsonObj.bf,"<br>")
+ngx.say(jsonObj.bf == nil,"<br>")
+
