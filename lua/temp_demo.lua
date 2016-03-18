69a110bd20f4d3899139df052fbc36a92684af29
tianyl1984 <tianyl1984@qq.com>
2016-03-18 15:26:56 +0800

    add template demo

---
+++ lua/temp_demo.lua
@@ -0,0 +1,11 @@
+local template = require("resty.template")
+template.caching(false)
+
+local stu = {
+	name = "张三",
+	age = 20,
+	male = true,
+	scores = {{name="数学",score=95},{name="语文",score=45},{name="英语",score=45.8}}
+}
+
+template.render("t1.html",stu)
